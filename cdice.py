97f0214148501f38765880ba2cc558b32af0da02
yawnoc <leeconway@protonmail.com>
2019-12-22 10:54:35 +0800

    Rename module without hyphen

--- cdice.py
+++ cdice.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 ################################################################
-# cantonese-diceware.py
+# cdice.py
 ################################################################
 # Generates Diceware lists of pronounceable Cantonese syllables
 # (including gibberish and pseudo-English).
